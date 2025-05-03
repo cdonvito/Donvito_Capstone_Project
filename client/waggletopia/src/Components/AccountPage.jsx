efacd2327c3ab49b78ef6a3e9a8ddf9028df4445
Craig Donvito <craigdonvito@MacBookPro.attlocal.net>
2025-05-03 18:10:54 -0400

    Adding Account page and api edits

--- client/waggletopia/src/Components/AccountPage.jsx
+++ client/waggletopia/src/Components/AccountPage.jsx
@@ -1,9 +1,36 @@
-function AccountPage () {
+import { useSelector } from "react-redux";
+import { useFetchUserMutation } from "./waggleApi";
+import { getToken } from "../Users/userSlice";
+
+function AccountPage() {
+  const [userDetails, { error, isLoading, isSuccess }] = useFetchUserMutation();
+  const token = useSelector(getToken);
+
+
+
+  // Show a loading message while data is being fetched
+  if (isLoading) {
+    return (
+      <section>
+        <h2>Loading...</h2>
+      </section>
+    );
+  }
+
+  // Show an error message if the fetch failed
+  if (error) {
+    return (
+      <section>
+        <h2>Error, please try again later.</h2>
+      </section>
+    );
+  }
+
   return (
-   <div>
-     <p>Account Page</p>
-   </div>
-  )
- };
- 
- export default AccountPage;
+    <div>
+      <p>Account Page</p>
+    </div>
+  );
+}
+
+export default AccountPage;
