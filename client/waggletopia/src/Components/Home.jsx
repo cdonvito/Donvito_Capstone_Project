3b80f1c2fc5e93c3a246e128c5baea516209e89a
Craig Donvito <craigdonvito@MacBookPro.attlocal.net>
2025-05-10 12:10:33 -0400

    Adding new product seed data

--- client/waggletopia/src/Components/Home.jsx
+++ client/waggletopia/src/Components/Home.jsx
@@ -1,7 +1,11 @@
+import dogHome from "../assets/dog_home.jpg"
+
 function Home () {
   return (
    <div>
-     <p>Home</p>
+     <button id="shopButton">Shop Our Products</button>
+
+     <img src={dogHome}></img>
    </div>
   )
  };
